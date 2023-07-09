3c4e131ee152c9c65d1a05064c1f48a95c79e633
Alexey Zaytsev <alex.rv9rh@gmail.com>
2023-07-09 20:07:59 +0500

    Start of adding auto_start_burn

--- ezcad_marking_control.py
+++ ezcad_marking_control.py
@@ -3,6 +3,7 @@ import os
 from pynput import keyboard
 import time
 import configparser
+from winGuiAuto import *
 
 
 CONFIG_FILE = "ezcad_marking_control.ini"
@@ -13,8 +14,62 @@ DEFAULT_CONFIG = {
     "window_title": "Маркировка цилиндров (вектор)",
     "element_title": "X",
     "shift_from_element": 1,
-    "switch_windows": True
+    "switch_windows": True,
+    "auto_start_burn": True,
 }
+
+# пока экспериментальный вариант (исправленный относительно winGuiAuto)
+# Из однострочного editText вроде извлекает значение нормально, с другими не проверял
+def getEditText_fixed(hwnd):
+    bufLen = win32gui.SendMessage(hwnd, win32con.WM_GETTEXTLENGTH, 0, 0) + 1
+    # print(bufLen)
+    buffer = win32gui.PyMakeBuffer(bufLen)
+    win32gui.SendMessage(hwnd, win32con.WM_GETTEXT, bufLen, buffer)
+    # text = buffer[:bufLen]
+    # text = win32gui.PyGetString(buffer.buffer_info()[0], bufLen - 1)
+    text = win32gui.PyGetString(win32gui.PyGetBufferAddressAndLen(buffer)[0], bufLen - 1)
+    return text
+
+def get_field_value(window_title, element_title, shift_from_element):
+    field_value = ""
+    result = {
+        "value": field_value,
+        "error": True
+    }
+    element_found = False
+
+    # найти окно
+    try:
+        window = findTopWindow(wantedText=window_title)
+        dump = dumpWindow(window)
+    except WinGuiAutoError:
+        print(f"Window '{window_title}' not found!")
+        # result["error"] = True
+        return result
+
+    # найти элемент
+    for obj in dump:
+        # print(obj)
+        if obj[1] == element_title:
+            i = dump.index(obj) + shift_from_element
+            print("element: ", dump[i])
+            edit = dump[i]
+            try:
+                field_value = getEditText_fixed(edit[0])
+                print("field_value: ", field_value)
+                element_found = True
+                # field_value = int(field_value)
+            except ValueError:
+                print("Incorrect EditText value!")
+                # result["error"] = True
+                return result
+
+    if element_found:
+        result["error"] = False
+        result["value"] = field_value
+
+    return result
+
 
 def load_config():
     def load_key(parser, key, type="str"):
@@ -49,6 +104,7 @@ def load_config():
     config["element_title"] = load_key(parser, "element_title")
     config["shift_from_element"] = load_key(parser, "shift_from_element", "int")
     config["switch_windows"] = load_key(parser, "switch_windows", "bool")
+    config["auto_start_burn"] = load_key(parser, "auto_start_burn", "bool")
 
     return config
 
@@ -73,6 +129,7 @@ def main():
     ctrl_l_count = 0
     alt_l_count = 0
     suspended = False
+    source_field_value = {}
 
     with keyboard.Events() as events:
         for event in events:
@@ -113,6 +170,10 @@ def main():
                 # print(event)
 
                 if event.key == keyboard.Key.esc:
+                    # если авто старт, то дождаться возврата ротора и нажать f2
+                    if config["auto_start_burn"]:
+                        print("f2")
+
                     if config["switch_windows"]:
                         press_alt_tab()
                 else:
@@ -128,6 +189,17 @@ def main():
                         if config["switch_windows"]:
                             press_alt_tab()
                             time.sleep(config["operations_delay"])
+
+                        if config["auto_start_burn"]:
+                            # запомнить исходное значение поля позиции ротора
+                            source_field_value = get_field_value(
+                                config["window_title"], 
+                                config["element_title"], 
+                                config["shift_from_element"]
+                            )
+                            if source_field_value["error"]:
+                               print("Auto start is unavailable: no value for tracking") 
+
                         press_f1()
 
 
