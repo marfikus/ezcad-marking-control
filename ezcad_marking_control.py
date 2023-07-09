d2d989f7682b93097361ffd716fb765d763b8ed4
Alexey Zaytsev <alex.rv9rh@gmail.com>
2023-07-09 06:26:05 +0500

    Add main(), refactoring..

--- ezcad_marking_control.py
+++ ezcad_marking_control.py
@@ -2,12 +2,10 @@
 from pynput import keyboard
 import time
 
-kb = keyboard.Controller()
 
-delay = 0.2
-ctrl_l_count = 0
-alt_l_count = 0
-suspended = False
+OPERATIONS_DELAY = 0.2
+
+kb = keyboard.Controller()
 
 def press_alt_tab():
     kb.press(keyboard.Key.alt)
@@ -20,56 +18,63 @@ def press_f1():
     kb.release(keyboard.Key.f1)
 
 
-with keyboard.Events() as events:
-    for event in events:
-        # print(event)
-        # print(event.key.char)
+def main():
+    ctrl_l_count = 0
+    alt_l_count = 0
+    suspended = False
 
-        if type(event) is not keyboard.Events.Release:
-            continue        
+    with keyboard.Events() as events:
+        for event in events:
+            # print(event)
+            # print(event.key.char)
 
-        # if type(event) is keyboard.Events.Release:
-        if event.key == keyboard.Key.ctrl_l:
-            alt_l_count = 0
-            ctrl_l_count += 1
+            if type(event) is not keyboard.Events.Release:
+                continue        
 
-            if ctrl_l_count == 2:
-                if not suspended:
-                    suspended = True
-                    print("program suspended")
-            elif ctrl_l_count == 4:
-                print("program stopped")
-                break
-        
-        elif event.key == keyboard.Key.alt_l:
-            ctrl_l_count = 0
-            alt_l_count += 1
+            if event.key == keyboard.Key.ctrl_l:
+                alt_l_count = 0
+                ctrl_l_count += 1
 
-            if alt_l_count == 2:
+                if ctrl_l_count == 2:
+                    if not suspended:
+                        suspended = True
+                        print("program suspended")
+                elif ctrl_l_count == 4:
+                    print("program stopped")
+                    return
+            
+            elif event.key == keyboard.Key.alt_l:
+                ctrl_l_count = 0
+                alt_l_count += 1
+
+                if alt_l_count == 2:
+                    if suspended:
+                        suspended = False
+                        alt_l_count = 0
+                        print("program resumed")
+            else:
+                ctrl_l_count = 0
+                alt_l_count = 0
+
                 if suspended:
-                    suspended = False
-                    alt_l_count = 0
-                    print("program resumed")
-        else:
-            ctrl_l_count = 0
-            alt_l_count = 0
+                    continue
 
-            if suspended:
-                continue
+                # print(event)
 
-            print(event)
+                if event.key == keyboard.Key.esc:
+                    press_alt_tab()
 
-            if event.key == keyboard.Key.esc:
+            char = ''
+            try:
+                char = event.key.char
+            except AttributeError:
+                pass
+
+            if char == '`' or char == '1': # потом сделать один вариант (настройка в конфиге)
                 press_alt_tab()
+                time.sleep(OPERATIONS_DELAY)
+                press_f1()
 
-        char = ''
-        try:
-            char = event.key.char
-        except AttributeError:
-            pass
 
-        if char == '`' or char == '1': # потом сделать один вариант (настройка в конфиге)
-            press_alt_tab()
-            time.sleep(delay)
-            press_f1()
-
+if __name__ == "__main__":
+    main()
